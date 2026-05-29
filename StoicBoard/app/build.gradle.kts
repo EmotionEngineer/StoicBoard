840ca1f77a02202748d4505c04ccfd371209b907
Islam Tlupov <70014075+EmotionEngineer@users.noreply.github.com>
2026-05-29 16:26:14 +0300

    SDK version up

--- StoicBoard/app/build.gradle.kts
+++ StoicBoard/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.primitives.stoicboard"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.primitives.stoicboard"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
         
